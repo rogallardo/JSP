222c346f3c6c3736c54f851501df5f4d6912ddc4
rogallardo <rodrigogastongallardo@gmail.com>
2022-04-29 17:30:06 -0300

    nueva act<

--- App.js
+++ App.js
@@ -28,20 +28,17 @@ const bici6 = new Bici(6,"MTB Cube Slyer 29'", "Shimano XT 10 1x10" , 520000,6)
 // Mi array que va a contener las bicis.
 const bicis =[bici1,bici2,bici3,bici4, bici5,bici6];
 const carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+
 datosCarrito()
+
 
 
 generadorDeCards(bicis)
 function generadorDeCards(bicis) {
     const clase= document.getElementById('productos-container')
-
     for(const bici of bicis){
-        // Defino un contenedor
-        // Este se va a crear por cada elemento de mi array.
         const contenedor = document.createElement('div')
-        // A este div le voy a poner una clase
         contenedor.className='item-prod-container'
-        // Y ahora voy a cargar para que se vean :D Voy a utilizar backtips para poder usar javascript dentro del string
         contenedor.innerHTML=`
                         <div id="MTB" class="item-prod ">
                         <div class="item-img-container">
@@ -53,57 +50,47 @@ function generadorDeCards(bicis) {
                             <h4 class="card-title">${bici.modelo}</h4>
                             <h5>${bici.equipamiento}</h5>
                             <h6>$${bici.precio}</h6>
-                            <a id= "agregarAlCarrito${bici.id}" class="btn-prod2">+ Agregar al carrito </a>
+                            <a onclick="agregarAlCarrito(${bici.id})" id= "agregCarrito${bici.id}" class="btn-prod2">+ Agregar al carrito </a>
                         </div> 
                     </div>`
-        // Luego le agrego al div grandote que declaramos primero que este div va a ser su hijo
         clase.append(contenedor)
-        document.getElementById(`agregarAlCarrito${bici.id}`).addEventListener('click', () => {
-            agregarAlCarrito(bici.id)
-        const btnAgregarCarrito = document.getElementById(`agregarAlCarrito${bici.id}`);
-        btnAgregarCarrito.innerHTML = `Agregado`
-        btnAgregarCarrito.className = "btn-prod3"
-        })
-        
     }
+    
 }
 
 const mostrarCardsCarrito = (cards) => document.getElementById("carrito").innerHTML = cards;
 generarCardsEnCarrito(carrito)
 function generarCardsEnCarrito(carrito) {
-    let acumuladorDeCards = ''
+    let acumuladorDeCards = ``
     carrito.forEach((bici) => {
         acumuladorDeCards +=`
-                        <div>
-                        <div id="MTB" class="item-prod ">
-                        <div class="item-img-container">
-                            <img class= "item-img" src="/img/prod${bici.id}.jpg" class="card-img-top" alt=
-                        </div>
-                        <div class="item-desc-container">
-                        <a onclick="eliminar(${bici.id})" class="btn-prod" >Eliminar</a>
-                            <h4 class="card-title">${bici.modelo}</h4>
-                            <h5>${bici.equipamiento}</h5>
-                            <h6>$${bici.precio}</h6>
-                            <div class="cantidad-container">
-                                
-                                <a onclick="dismCant(${bici.id})" id= "cantidad-${bici.id}" class="btn-cant">-</a>
-                                
-                                <a  id="contadorCant${bici.id}" class="cant"> ${bici.cantidad} </a>
-                                
-                                <a onclick="aumentCant(${bici.id})" id= "cantidad+${bici.id}" class ="btn-cant">+ </a>
-                                
-                            
+                        
+                        <div id="MTB" class="item-prod-carrito ">
+                            <div class="item-img-container-carrito">
+                            <img class= "item-img-carrito" src="/img/prod${bici.id}.jpg" class="card-img-top" alt=
                             </div>
-                            
-                        </div>
-                    </div>
-                    </div>`
+                            <div class="item-desc-container-carrito">
+                                <a onclick="eliminar(${bici.id})" class="btn-prod" >Eliminar</a>
+                                <h4 class="card-title">${bici.modelo}</h4>
+                                <h5>${bici.equipamiento}</h5>
+                                <h6>$${bici.precio}</h6>
+                                <div class="cantidad-container">
+                                    <a onclick="dismCant(${bici.id})" id= "cantidad-${bici.id}" class="btn-cant1">-</a>
+                                    <a  id="contadorCant${bici.id}" class="cant"> ${bici.cantidad} </a>
+                                    <a onclick="aumentCant(${bici.id})" id= "cantidad+${bici.id}" class ="btn-cant2">+ </a>                          
+                                </div>
+                            </div>
+                        </div>`
+
+    btnAgregado (bici.id)    
  }) 
   
     mostrarCardsCarrito(acumuladorDeCards)
+    
+    
 }
-    
-    
+
+
 
 function cantidadTotalCarrito (){  
     const totalProductos = carrito.reduce((acc, producto) => (acc + producto.cantidad), 0)
@@ -119,18 +106,13 @@ function datosCarrito() {
     cantidadTotalCarrito()
     precioTotal()
     
+    
     localStorage.setItem("carrito", JSON.stringify(carrito))
 }
 
 
-
-
-
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //--------------------------------------------------- BOTONES ----------------------------------------------------------------------------------------//
-
-
 
 function agregarAlCarrito(idProducto){
     const productoAgregado = bicis.find(el => el.id === idProducto)
@@ -139,13 +121,26 @@ function agregarAlCarrito(idProducto){
             carrito.push(productoAgregado);
             productoAgregado.stock--;
             productoAgregado.cantidad++; 
-            generarCardsEnCarrito(carrito)
-    
+            generarCardsEnCarrito(carrito)      
     }
-    datosCarrito()    
+    datosCarrito()
 }
 
 
+function btnAgregado (idProducto) {
+    carrito.find(el => el.id === idProducto)
+    const btnAgregado = document.getElementById(`agregCarrito${idProducto}`);
+    btnAgregado.innerHTML =`Agregado`;
+    btnAgregado.className = `btn-prod3`
+    
+}
+function btnAgregarAlCarrito (idProducto) {
+    carrito.find(el => el.id === idProducto)
+    const btnAgregado = document.getElementById(`agregCarrito${idProducto}`);
+    btnAgregado.innerHTML =`+ Agregar al carrito`;
+    btnAgregado.className = `btn-prod2`
+    
+}
 
 function eliminar(idProducto){
     const productoAEliminar = carrito.find(el=> el.id === idProducto)
@@ -155,6 +150,7 @@ function eliminar(idProducto){
     productoAEliminar.cantidad = 0;
     generarCardsEnCarrito(carrito)  
     datosCarrito()
+    btnAgregarAlCarrito (idProducto)
 }
 
 function noHayStock() {
@@ -179,7 +175,6 @@ function aumentCant(idProducto){
     datosCarrito()
  
 }
-
 function dismCant(idProducto){          
     const productoADisminuir= carrito.find(el => el.id === idProducto)
     if(productoADisminuir.cantidad > 1){
